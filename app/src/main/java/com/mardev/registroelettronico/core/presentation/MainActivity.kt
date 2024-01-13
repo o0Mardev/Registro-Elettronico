8fb4d304c24617c9531f94d997810e6f28b22e98
Marco <marco1.delvecchio@gmail.com>
2024-01-13 23:01:42 +0100

    Integrated UserSettings in MainActivity

--- app/src/main/java/com/mardev/registroelettronico/core/presentation/MainActivity.kt
+++ app/src/main/java/com/mardev/registroelettronico/core/presentation/MainActivity.kt
@@ -7,29 +7,44 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
-import com.mardev.registroelettronico.core.presentation.AppState
-import com.mardev.registroelettronico.core.presentation.rememberAppState
 import com.mardev.registroelettronico.feature_authentication.presentation.login_screen.components.LoginScreen
 import com.mardev.registroelettronico.feature_authentication.presentation.search_screen.components.SearchScreen
 import com.mardev.registroelettronico.feature_main.presentation.components.MainScreen
+import com.mardev.registroelettronico.feature_settings.presentation.AppTheme
+import com.mardev.registroelettronico.feature_settings.presentation.UserSettings
 import com.mardev.registroelettronico.ui.theme.RegistroElettronicoTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var userSettings: UserSettings
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val theme = userSettings.themeStream.collectAsState()
+            val isDarkMode = when(theme.value){
+                AppTheme.MODE_AUTO -> isSystemInDarkTheme()
+                AppTheme.MODE_DAY -> false
+                AppTheme.MODE_NIGHT -> true
+            }
+
             val appState: AppState = rememberAppState()
             val navController = rememberNavController()
+
             RegistroElettronicoTheme(
-                darkTheme = appState.isDarkMode.value
+                darkTheme = isDarkMode
             ) {
                 NavHost(
                     navController = navController, startDestination = "authGraph",
@@ -61,7 +76,7 @@ class MainActivity : ComponentActivity() {
                         }
                     ) {
                         composable("home") {
-                            MainScreen(appState)
+                            MainScreen(appState, userSettings)
                         }
                     }
                 }
