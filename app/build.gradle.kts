fc8c0807328c675dc9ef84d07e8c718b29d702d0
Marco <marco1.delvecchio@gmail.com>
2024-01-13 22:58:22 +0100

    Upgraded dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,7 @@ dependencies {
     implementation ("androidx.compose.material:material:1.5.4")
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
