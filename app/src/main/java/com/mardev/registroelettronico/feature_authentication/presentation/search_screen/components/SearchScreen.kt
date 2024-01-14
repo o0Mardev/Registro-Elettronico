fab49fa23ce779e02f641381068391117b3099cd
Marco <marco1.delvecchio@gmail.com>
2024-01-14 11:25:33 +0100

    Redesigned SearchScreen

--- app/src/main/java/com/mardev/registroelettronico/feature_authentication/presentation/search_screen/components/SearchScreen.kt
+++ app/src/main/java/com/mardev/registroelettronico/feature_authentication/presentation/search_screen/components/SearchScreen.kt
@@ -1,12 +1,9 @@
 package com.mardev.registroelettronico.feature_authentication.presentation.search_screen.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -14,77 +11,68 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DividerDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SearchBar
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.mardev.registroelettronico.feature_authentication.presentation.search_screen.SearchViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(navController: NavController) {
     val viewModel: SearchViewModel = hiltViewModel()
     val searchText by viewModel.searchText.collectAsState()
     val schools by viewModel.schools.collectAsState(emptyList())
-    var active by remember {
-        mutableStateOf(false)
-    }
 
-    SearchBar(modifier = Modifier.fillMaxWidth(),
-        query = searchText,
-        onQueryChange = viewModel::onSearchTextChange,
-        onSearch = {},
-        active = active,
-        onActiveChange = {
-            active = it
-        },
-        leadingIcon = {
-            IconButton(onClick = {
-                navController.popBackStack()
-            }) {
-                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
-            }
-        },
-        trailingIcon = {
-            if (active) {
-                Icon(imageVector = Icons.Default.Close,
-                    contentDescription = null,
-                    modifier = Modifier.clickable {
-                        active = false
-                    })
-            }
-        },
-        placeholder = {
-            Text(text = "Cerca la tua scuola")
-        }) {
-        LazyColumn {
-            items(schools, { school ->
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        OutlinedTextField(
+            supportingText = {
+                Text(text = "Inserisci il nome della scuola, il comune o il CAP")
+
+            },
+            modifier = Modifier.fillMaxWidth(),
+            shape = MaterialTheme.shapes.extraLarge,
+            placeholder = { Text(text = "Cerca la tua scuola") },
+            leadingIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                }
+            },
+            trailingIcon = {
+                if (searchText.isNotEmpty()) {
+                    IconButton(onClick = {
+                        viewModel.onCancelClick()
+                    }) {
+                        Icon(imageVector = Icons.Default.Close, contentDescription = null)
+                    }
+                }
+            },
+            singleLine = true,
+            value = searchText,
+            onValueChange = viewModel::onSearchTextChange
+        )
+        LazyColumn(modifier = Modifier.padding(4.dp)) {
+            items(items = schools, key = { school ->
                 school.taxCode
             }) { school ->
-                SchoolItem(school = school){ taxCode ->
+                SchoolItem(school = school) { taxCode ->
                     navController.previousBackStackEntry?.savedStateHandle?.set("taxCode", taxCode)
                     navController.popBackStack()
                 }
-                Spacer(modifier = Modifier.height(8.dp))
-                Divider(thickness = DividerDefaults.Thickness.times(3))
+                Divider(thickness = DividerDefaults.Thickness.times(2))
             }
         }
     }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-    )
 }
