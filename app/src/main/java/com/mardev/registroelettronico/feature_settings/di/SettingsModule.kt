9b75fd54702f50aa03adcd95141706e0c1060c2e
Marco <marco1.delvecchio@gmail.com>
2024-01-14 11:21:24 +0100

    Moved provideUserPreferencesDataStoreRepository to SettingsModule

--- app/src/main/java/com/mardev/registroelettronico/feature_settings/di/SettingsModule.kt
+++ app/src/main/java/com/mardev/registroelettronico/feature_settings/di/SettingsModule.kt
@@ -1,11 +1,9 @@
-package com.mardev.registroelettronico.core.di
+package com.mardev.registroelettronico.feature_settings.di
 
 import android.content.Context
 import com.mardev.registroelettronico.core.data.repository.DataStoreRepositoryImpl
 import com.mardev.registroelettronico.core.domain.repository.DataStoreRepository
 import com.mardev.registroelettronico.core.util.Constants
-import com.mardev.registroelettronico.feature_authentication.data.repository.SessionCacheImpl
-import com.mardev.registroelettronico.feature_authentication.domain.repository.SessionCache
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,19 +14,14 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
+object SettingsModule {
     @Provides
     @Singleton
-    @Named("userSessionDatastore")
-    fun provideUserSessionDataStoreRepository(
+    @Named("userPreferencesDatastore")
+    fun provideUserPreferencesDataStoreRepository(
         @ApplicationContext app: Context
     ): DataStoreRepository {
-        return DataStoreRepositoryImpl(app, Constants.userSessionDatastoreName)
+        return DataStoreRepositoryImpl(app, Constants.userPreferencesDatastoreName)
     }
 
-    @Provides
-    @Singleton
-    fun provideSessionCache(@Named("userSessionDatastore") repository: DataStoreRepository): SessionCache {
-        return SessionCacheImpl(repository)
-    }
 }
