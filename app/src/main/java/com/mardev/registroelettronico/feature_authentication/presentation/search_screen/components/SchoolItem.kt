f59cb714b74f06c5fc4b4fd333d72f0773357131
Marco <marco1.delvecchio@gmail.com>
2024-01-14 11:39:11 +0100

    Changed SchoolItem text color

--- app/src/main/java/com/mardev/registroelettronico/feature_authentication/presentation/search_screen/components/SchoolItem.kt
+++ app/src/main/java/com/mardev/registroelettronico/feature_authentication/presentation/search_screen/components/SchoolItem.kt
@@ -3,17 +3,20 @@ package com.mardev.registroelettronico.feature_authentication.presentation.searc
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.mardev.registroelettronico.feature_authentication.domain.model.School
 
 @Composable
 fun SchoolItem(school: School, onClick: (String) -> Unit) {
-    Column(modifier = Modifier.fillMaxWidth().clickable {
+    Column(modifier = Modifier.fillMaxWidth().padding(4.dp).clickable {
         onClick(school.taxCode)
     }) {
-            Text(text = "${school.type} ${school.name}")
-            Text(text = "${school.region} ${school.city} ${school.province}")
+            Text(text = "${school.type} ${school.name}", color = MaterialTheme.colorScheme.onBackground)
+            Text(text = "${school.region} ${school.city} ${school.province}", color = MaterialTheme.colorScheme.onBackground)
     }
 }
