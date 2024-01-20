4d34fff014991043e9c10037306576c11e681210
Marco <marco1.delvecchio@gmail.com>
2024-01-20 22:20:10 +0100

    Using fixed width for login button

--- app/src/main/java/com/mardev/registroelettronico/feature_authentication/presentation/login_screen/components/LoginScreen.kt
+++ app/src/main/java/com/mardev/registroelettronico/feature_authentication/presentation/login_screen/components/LoginScreen.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -27,7 +27,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -153,7 +152,7 @@ fun LoginScreen(
                 Button(
                     enabled = !state.isLoading,
                     modifier = Modifier
-                        .fillMaxWidth(0.5f)
+                        .width(250.dp)
                         .height(50.dp),
                     onClick = {
                         viewModel.onLogin()
