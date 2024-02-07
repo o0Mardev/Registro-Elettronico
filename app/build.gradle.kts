6953c6ae9ad892f74c509f40f8b6404523d7d0d7
Marco <marco1.delvecchio@gmail.com>
2024-02-07 18:25:17 +0100

    Reverted dependencies version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.6")
 
     // Compose icons
-    implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 
     // Kotlin coroutines
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
@@ -101,7 +101,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Material 3
-    implementation ("androidx.compose.material:material:1.6.0")
+    implementation ("androidx.compose.material:material:1.5.4")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
