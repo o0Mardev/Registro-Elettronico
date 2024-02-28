d53866be5152ef328deb16d014565a697e137e06
Marco <marco1.delvecchio@gmail.com>
2024-02-28 18:52:11 +0100

    Changed base url to family one

--- app/src/main/java/com/mardev/registroelettronico/core/util/Constants.kt
+++ app/src/main/java/com/mardev/registroelettronico/core/util/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val vendorToken: String = "5ed95c58-fbc2-4db8-92cb-7e1e73ba2065"
     const val sAppName: String = "ALU_APP"
     const val rc4Key: String = "Cf4G8nepTqwyp2Y4suvQMdQJ"
-    const val BASE_URL = "https://wsalu.axioscloud.it/webservice/AxiosCloud_Ws_Rest.svc/"
+    const val BASE_URL = "https://wsfam.axioscloud.it/webservice/AxiosCloud_Ws_Rest.svc/"
     const val BASE_URL_APP_UPDATE_API = "https://raw.githubusercontent.com/o0Mardev/Registro-Elettronico/master/"
     const val userPreferencesDatastoreName = "user_preferences"
     const val userSessionDatastoreName = "user_session"
