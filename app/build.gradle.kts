8c3499e04b699c736a4da190ed1828e1eacb31c5
Marco <marco1.delvecchio@gmail.com>
2024-06-09 17:42:13 +0200

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,10 +71,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 
     // To debug compose recomposition
-    implementation("io.github.theapache64:rebugger:1.0.0-rc02")
+    implementation("io.github.theapache64:rebugger:1.0.0-rc03")
 
     // To parse html received from the api
-    implementation("org.jsoup:jsoup:1.17.1")
+    implementation("org.jsoup:jsoup:1.17.2")
 
     // Room
     implementation("androidx.room:room-runtime:2.6.1")
@@ -84,13 +84,13 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
 
     // Datastore
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     // Compose navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Compose icons
-    implementation("androidx.compose.material:material-icons-extended:1.6.4")
+    implementation("androidx.compose.material:material-icons-extended")
 
     // Kotlin coroutines
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
@@ -101,16 +101,16 @@ dependencies {
     kapt("com.google.dagger:hilt-android-compiler:2.48.1")
 
     // Retrofit dependencies
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
     // Material 3
     implementation ("androidx.compose.material:material")
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -118,7 +118,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
