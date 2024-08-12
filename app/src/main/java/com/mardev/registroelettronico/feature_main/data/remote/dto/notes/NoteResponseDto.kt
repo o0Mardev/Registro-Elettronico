ca7030a2b005c4fdec562232c1320772831f1eee
Marco <marco1.delvecchio@gmail.com>
2024-08-12 10:05:32 +0200

    Removed nullable error message

--- app/src/main/java/com/mardev/registroelettronico/feature_main/data/remote/dto/notes/NoteResponseDto.kt
+++ app/src/main/java/com/mardev/registroelettronico/feature_main/data/remote/dto/notes/NoteResponseDto.kt
@@ -2,6 +2,6 @@ package com.mardev.registroelettronico.feature_main.data.remote.dto.notes
 
 data class NoteResponseDto(
     val errorcode: Int,
-    val errormessage: String?,
+    val errormessage: String,
     val response: List<NoteDataDto>?
 )
