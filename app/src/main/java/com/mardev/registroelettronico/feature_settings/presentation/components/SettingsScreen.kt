4dd6a2b31302300653b97aad2c965cf68b981d38
Marco <marco1.delvecchio@gmail.com>
2024-08-30 19:03:37 +0200

    Implemented edge-to-edge

--- app/src/main/java/com/mardev/registroelettronico/feature_settings/presentation/components/SettingsScreen.kt
+++ app/src/main/java/com/mardev/registroelettronico/feature_settings/presentation/components/SettingsScreen.kt
@@ -1,8 +1,13 @@
 package com.mardev.registroelettronico.feature_settings.presentation.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -15,6 +20,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import com.mardev.registroelettronico.feature_settings.presentation.AppTheme
 import com.mardev.registroelettronico.feature_settings.presentation.AppTheme.Companion.resourceIdStringFromAppTheme
@@ -46,7 +52,12 @@ fun SettingsScreen(userSettings: UserSettings) {
         Column(
             Modifier
                 .verticalScroll(rememberScrollState())
-                .consumeWindowInsets(paddingValues),
+                .consumeWindowInsets(paddingValues).then(
+                    if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        Modifier
+                            .windowInsetsPadding(WindowInsets.navigationBars)
+                            .windowInsetsPadding(WindowInsets.displayCutout)
+                    } else Modifier),
             verticalArrangement = Arrangement.SpaceEvenly
         ) {
 
