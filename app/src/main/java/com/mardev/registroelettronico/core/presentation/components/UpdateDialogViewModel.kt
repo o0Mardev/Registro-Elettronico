22585d77a342ce0293795c110d01e5dbc9bd3efa
Marco <marco1.delvecchio@gmail.com>
2024-08-30 18:55:13 +0200

    Changed from just Log.d to Timber logging

--- app/src/main/java/com/mardev/registroelettronico/core/presentation/components/UpdateDialogViewModel.kt
+++ app/src/main/java/com/mardev/registroelettronico/core/presentation/components/UpdateDialogViewModel.kt
@@ -1,7 +1,6 @@
 package com.mardev.registroelettronico.core.presentation.components
 
 import android.content.Context
-import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
