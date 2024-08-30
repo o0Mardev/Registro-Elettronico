a2450968ea34bfa30d50abe161db52a44e8906c9
Marco <marco1.delvecchio@gmail.com>
2024-08-30 19:13:52 +0200

    Implemented edge-to-edge

--- app/src/main/java/com/mardev/registroelettronico/feature_main/presentation/components/absence_screen/AbsenceScreen.kt
+++ app/src/main/java/com/mardev/registroelettronico/feature_main/presentation/components/absence_screen/AbsenceScreen.kt
@@ -1,12 +1,18 @@
 package com.mardev.registroelettronico.feature_main.presentation.components.absence_screen
 
+import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsBottomHeight
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.DividerDefaults
@@ -23,6 +29,8 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.mardev.registroelettronico.feature_main.presentation.components.common.DateItem
 
@@ -61,69 +69,131 @@ fun AbsenceScreen(
         when (selectedTabIndex) {
             0 -> {
                 val groupedAbsences = state.absences.groupBy { it.date }
-                LazyColumn(modifier = Modifier.padding(paddingValues)) {
-                    groupedAbsences.forEach { (header, items) ->
-                        stickyHeader {
-                            DateItem(
-                                date = header,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(MaterialTheme.colorScheme.secondaryContainer),
-                                style = MaterialTheme.typography.headlineSmall
+                LazyColumn(modifier = Modifier.padding(top = paddingValues.calculateTopPadding(), start = 2.dp, end = 2.dp).then(
+                    if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        Modifier
+                            .windowInsetsPadding(WindowInsets.navigationBars)
+                            .windowInsetsPadding(WindowInsets.displayCutout)
+                    } else Modifier)) {
+                    if (groupedAbsences.isEmpty()) {
+                        item {
+                            Text(
+                                text = "Non sono presenti assenze per il seguente periodo.",
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.Center
                             )
                         }
-                        items(items, key = { item -> item.id }) { absence ->
-                            Spacer(modifier = Modifier.height(4.dp))
-                            AbsenceItem(genericAbsence = absence)
-                            Spacer(modifier = Modifier.height(4.dp))
-                            HorizontalDivider(thickness = DividerDefaults.Thickness.times(2))
-                            Spacer(modifier = Modifier.height(4.dp))
+                    } else {
+                        groupedAbsences.forEach { (header, items) ->
+                            stickyHeader {
+                                DateItem(
+                                    date = header,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(top = 4.dp)
+                                        .background(MaterialTheme.colorScheme.secondaryContainer),
+                                    style = MaterialTheme.typography.headlineSmall
+                                )
+                            }
+                            items(items, key = { item -> item.id }) { absence ->
+                                AbsenceItem(genericAbsence = absence)
+                                Spacer(modifier = Modifier.height(4.dp))
+                                HorizontalDivider(thickness = DividerDefaults.Thickness.times(2))
+                            }
+                        }
+                        item {
+                            Spacer(
+                                modifier = Modifier
+                                    .windowInsetsBottomHeight(WindowInsets.navigationBars)
+                            )
                         }
                     }
                 }
             }
+
             1 -> {
                 val groupedDelays = state.delays.groupBy { it.date }
-                LazyColumn(modifier = Modifier.padding(paddingValues)) {
-                    groupedDelays.forEach { (header, items) ->
-                        stickyHeader {
-                            DateItem(
-                                date = header,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(MaterialTheme.colorScheme.secondaryContainer),
-                                style = MaterialTheme.typography.headlineSmall
+                LazyColumn(modifier = Modifier.padding(top = paddingValues.calculateTopPadding(), start = 2.dp, end = 2.dp).then(
+                    if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        Modifier
+                            .windowInsetsPadding(WindowInsets.navigationBars)
+                            .windowInsetsPadding(WindowInsets.displayCutout)
+                    } else Modifier)) {
+                    if (groupedDelays.isEmpty()) {
+                        item {
+                            Text(
+                                text = "Non sono presenti ritardi per il seguente periodo.",
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.Center
                             )
                         }
-                        items(items, key = { item -> item.id }) { absence ->
-                            Spacer(modifier = Modifier.height(4.dp))
-                            AbsenceItem(genericAbsence = absence)
-                            Spacer(modifier = Modifier.height(4.dp))
-                            HorizontalDivider(thickness = DividerDefaults.Thickness.times(2))
-                            Spacer(modifier = Modifier.height(4.dp))
+                    } else {
+                        groupedDelays.forEach { (header, items) ->
+                            stickyHeader {
+                                DateItem(
+                                    date = header,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(top = 4.dp)
+                                        .background(MaterialTheme.colorScheme.secondaryContainer),
+                                    style = MaterialTheme.typography.headlineSmall
+                                )
+                            }
+                            items(items, key = { item -> item.id }) { absence ->
+                                AbsenceItem(genericAbsence = absence)
+                                Spacer(modifier = Modifier.height(4.dp))
+                                HorizontalDivider(thickness = DividerDefaults.Thickness.times(2))
+                            }
+                        }
+                        item {
+                            Spacer(
+                                modifier = Modifier
+                                    .windowInsetsBottomHeight(WindowInsets.navigationBars)
+                            )
                         }
                     }
                 }
             }
+
             2 -> {
                 val groupedExits = state.exits.groupBy { it.date }
-                LazyColumn(modifier = Modifier.padding(paddingValues)) {
-                    groupedExits.forEach { (header, items) ->
-                        stickyHeader {
-                            DateItem(
-                                date = header,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(MaterialTheme.colorScheme.secondaryContainer),
-                                style = MaterialTheme.typography.headlineSmall
+                LazyColumn(modifier = Modifier.padding(top = paddingValues.calculateTopPadding(), start = 2.dp, end = 2.dp).then(
+                        if (LocalConfiguration.current.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                            Modifier
+                                .windowInsetsPadding(WindowInsets.navigationBars)
+                                .windowInsetsPadding(WindowInsets.displayCutout)
+                        } else Modifier)) {
+                    if (groupedExits.isEmpty()) {
+                        item {
+                            Text(
+                                text = "Non sono presenti uscite per il seguente periodo.",
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.Center
                             )
                         }
-                        items(items, key = { item -> item.id }) { absence ->
-                            Spacer(modifier = Modifier.height(4.dp))
-                            AbsenceItem(genericAbsence = absence)
-                            Spacer(modifier = Modifier.height(4.dp))
-                            HorizontalDivider(thickness = DividerDefaults.Thickness.times(2))
-                            Spacer(modifier = Modifier.height(4.dp))
+                    } else {
+                        groupedExits.forEach { (header, items) ->
+                            stickyHeader {
+                                DateItem(
+                                    date = header,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(top = 4.dp)
+                                        .background(MaterialTheme.colorScheme.secondaryContainer),
+                                    style = MaterialTheme.typography.headlineSmall
+                                )
+                            }
+                            items(items, key = { item -> item.id }) { absence ->
+                                AbsenceItem(genericAbsence = absence)
+                                Spacer(modifier = Modifier.height(4.dp))
+                                HorizontalDivider(thickness = DividerDefaults.Thickness.times(2))
+                            }
+                        }
+                        item {
+                            Spacer(
+                                modifier = Modifier
+                                    .windowInsetsBottomHeight(WindowInsets.navigationBars)
+                            )
                         }
                     }
                 }
