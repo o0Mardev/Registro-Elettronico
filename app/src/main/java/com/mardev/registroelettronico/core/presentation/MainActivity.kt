4426f32069972e71ce936b78cbcb2837559e6f37
Marco <marco1.delvecchio@gmail.com>
2024-08-31 15:30:20 +0200

    Global snackbar

--- app/src/main/java/com/mardev/registroelettronico/core/presentation/MainActivity.kt
+++ app/src/main/java/com/mardev/registroelettronico/core/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mardev.registroelettronico.core.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,9 +10,18 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
@@ -27,6 +37,7 @@ import com.mardev.registroelettronico.feature_settings.presentation.AppTheme
 import com.mardev.registroelettronico.feature_settings.presentation.UserSettings
 import com.mardev.registroelettronico.ui.theme.RegistroElettronicoTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -35,6 +46,7 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var userSettings: UserSettings
 
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -42,7 +54,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val theme = userSettings.themeStream.collectAsState()
-            val isDarkMode = when(theme.value){
+            val isDarkMode = when (theme.value) {
                 AppTheme.MODE_AUTO -> isSystemInDarkTheme()
                 AppTheme.MODE_DAY -> false
                 AppTheme.MODE_NIGHT -> true
@@ -50,50 +62,78 @@ class MainActivity : ComponentActivity() {
 
             val dynamicColor = userSettings.dynamicColorStream.collectAsState()
 
-            val appState: AppState = rememberAppState()
             val navController = rememberNavController()
 
             RegistroElettronicoTheme(
                 darkTheme = isDarkMode,
                 dynamicColor = dynamicColor.value
             ) {
-                UpdateDialog()
-                NavHost(
-                    navController = navController, startDestination = "authGraph",
-                    modifier = Modifier.background(MaterialTheme.colorScheme.background)
-                ) {
-                    navigation(
-                        startDestination = "login",
-                        route = "authGraph"
-                    ) {
-                        composable("login") { entry ->
-                            val retrievedTaxCode = entry.savedStateHandle.get<String>("taxCode")
-                            entry.savedStateHandle.remove<String>("taxCode")
-                            LoginScreen(navController, appState, retrievedTaxCode)
-                        }
-                        composable("search") {
-                            SearchScreen(navController)
-                        }
-                    }
-                    navigation(
-                        startDestination = "home",
-                        route = "mainGraph",
-                        enterTransition = {
-                            slideIntoContainer(
-                                towards = AnimatedContentTransitionScope.SlideDirection.Companion.Up,
-                                animationSpec = spring(
-                                    Spring.DampingRatioLowBouncy,
-                                    Spring.StiffnessLow
-                                )
-                            )
-                        }
-                    ) {
-                        composable("home") {
-                            val viewModel: MainViewModel = hiltViewModel()
-                            MainScreen(appState, userSettings, viewModel)
+
+                val snackbarHostState = remember {
+                    SnackbarHostState()
+                }
+
+                val scope = rememberCoroutineScope()
+                val context = LocalContext.current
+                ObserveAsEvents(flow = SnackbarController.events, snackbarHostState) { event ->
+                    scope.launch {
+                        val result = snackbarHostState.showSnackbar(
+                            message = event.message.asString(context),
+                            actionLabel = event.action?.name,
+                            duration = SnackbarDuration.Short
+                        )
+
+                        if (result == SnackbarResult.ActionPerformed) {
+                            event.action?.action?.invoke()
                         }
                     }
                 }
+
+                UpdateDialog()
+
+                Scaffold(
+                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    NavHost(
+                        navController = navController, startDestination = "authGraph",
+                        modifier = Modifier
+                            .background(MaterialTheme.colorScheme.background)
+                    ) {
+                        navigation(
+                            startDestination = "login",
+                            route = "authGraph"
+                        ) {
+                            composable("login") { entry ->
+                                val retrievedTaxCode = entry.savedStateHandle.get<String>("taxCode")
+                                entry.savedStateHandle.remove<String>("taxCode")
+                                LoginScreen(navController, retrievedTaxCode)
+                            }
+                            composable("search") {
+                                SearchScreen(navController)
+                            }
+                        }
+                        navigation(
+                            startDestination = "home",
+                            route = "mainGraph",
+                            enterTransition = {
+                                slideIntoContainer(
+                                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Up,
+                                    animationSpec = spring(
+                                        Spring.DampingRatioLowBouncy,
+                                        Spring.StiffnessLow
+                                    )
+                                )
+                            }
+                        ) {
+                            composable("home") {
+                                val viewModel: MainViewModel = hiltViewModel()
+                                MainScreen(userSettings, viewModel)
+                            }
+                        }
+                    }
+                }
+
             }
         }
     }
