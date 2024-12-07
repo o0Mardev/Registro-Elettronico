3ba8a30e65a7debc44b099ea29bb29c9e4b70509
Marco <marco1.delvecchio@gmail.com>
2024-12-07 18:34:23 +0100

    New version!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.mardev.registroelettronico"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
