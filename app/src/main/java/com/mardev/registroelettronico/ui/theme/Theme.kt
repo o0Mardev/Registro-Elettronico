3c63e8568be1ffed7c4c528c6e44089a922fb2f3
Marco <marco1.delvecchio@gmail.com>
2025-02-01 12:49:11 +0100

    Removed deprecated functions

--- app/src/main/java/com/mardev/registroelettronico/ui/theme/Theme.kt
+++ app/src/main/java/com/mardev/registroelettronico/ui/theme/Theme.kt
@@ -59,8 +59,6 @@ fun RegistroElettronicoTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = Color.Transparent.toArgb()
-            window.navigationBarColor = Color.Transparent.toArgb()
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 window.isNavigationBarContrastEnforced = false
